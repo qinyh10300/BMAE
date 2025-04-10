98deb11beab5bfaa8041b179d70e94a90474081c
Yihua Qin <qinyh10300@gmail.com>
2025-04-10 22:05:40 +0800

    add train bash file

--- models_mae.py
+++ models_mae.py
@@ -149,6 +149,7 @@ class MaskedAutoencoderViT(nn.Module):
 
     def forward_encoder(self, x, mask_ratio):
         # embed patches
+        # print(x.shape)
         x = self.patch_embed(x)
 
         # add pos embed w/o cls token
@@ -219,6 +220,13 @@ class MaskedAutoencoderViT(nn.Module):
         loss = self.forward_loss(imgs, pred, mask)
         return loss, pred, mask
 
+def mae_vit_tiny_patch4_dec512d8b(**kwargs):
+    model = MaskedAutoencoderViT(
+        img_size=32, patch_size=4, embed_dim=192, depth=12, num_heads=3,
+        decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+        mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
 
 def mae_vit_base_patch16_dec512d8b(**kwargs):
     model = MaskedAutoencoderViT(
@@ -245,6 +253,7 @@ def mae_vit_huge_patch14_dec512d8b(**kwargs):
 
 
 # set recommended archs
+mae_vit_tiny_patch4 = mae_vit_tiny_patch4_dec512d8b  # decoder: 512 dim, 8 blocks
 mae_vit_base_patch16 = mae_vit_base_patch16_dec512d8b  # decoder: 512 dim, 8 blocks
 mae_vit_large_patch16 = mae_vit_large_patch16_dec512d8b  # decoder: 512 dim, 8 blocks
 mae_vit_huge_patch14 = mae_vit_huge_patch14_dec512d8b  # decoder: 512 dim, 8 blocks
